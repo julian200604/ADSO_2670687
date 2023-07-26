f7733c7bd22da852a4c6cacd545f699f762e0d0b
Julian200604 <hector_guillen@soy.sena.edu.co>
2023-07-25 21:46:47 -0500

    se agregaron cambios y javascript

---
+++ JAVASCRIPT/ejercicio 1/js/script.js
@@ -0,0 +1,53 @@
+//console.log("Analisis y desarrollo de software")
+//alert("Analisis y desarrollo de software")para una sola linea de codigo
+/*este tipo de comentario se trabaja en diferentes lineas de codigo*/
+
+//document.write("Analisis y desarrollo de software")
+
+//declarar variables var 
+
+//var edad=33;
+//const nom="héctor"
+//let num=2.5;
+//boleano=true;
+
+/*unir dos cadenas de texto
+const nom="héctor"
+const ape="guillén"
+alert("mi nombre es "+nom+" y mi apellido es "+ape)*/
+
+/*
+const num1=5
+const num2=5
+const div = num1/num2
+alert("la suma es:"+div)*/
+
+//let nom=prompt("Escriba el nombre")
+//alert("mi nombre es:" +nom)
+
+/*mostrar en una alerta con los nombres y apellidos completos, edad, peso y dirección*/
+
+let nom=prompt("Escriba sus nombres")
+let ape=prompt("Escriba sus apellidos")
+let eda=prompt("Escriba su edad")
+let pes=prompt("Escriba su peso")
+let dir=prompt("Escriba su dirección")
+
+alert("Mis nombre es "+nom+ " "+ape+" tengo "+eda+" años de edad, peso "+pes+" kilogramos "+" y mi dirección es "+dir)
+
+
+
+
+//alert("la suma es:"+parseInt(num1)+parseInt(num2))
+
+//console.log(typeof boleano);  //typeof es para definir tipo de dato */
+
+
+//console.log("mi eadad:"+edad);
+//alert("mi eadad:"+edad);
+//document.write("mi edad es:"+edad)
+//simbolo "+" sirve para concatenar
+
+//const edad=33;
+
+//let edad=33;
---
+++ JAVASCRIPT/ejercicio 2/js/script2.js
@@ -0,0 +1,14 @@
+/*Ejercicio 2- Dado el sueldo de un empleado imprimir en un alerta el nuevo suledo de un empleado si tuvo un aumento del 10% 
+
+let sueldo = prompt("Ingrese el valor del sueldo");
+let psueldo = 10/100;
+let nsueldo = parseInt(sueldo) + parseInt(sueldo*psueldo);
+alert("El nuevo sueldo con el aumento es: "+nsueldo); */
+
+/*Ejercicio 3- Un vendedor que tiene su sueldo base , recibe una comisión del 30% del total de las ventas del mes, el quiere saber
+cuanto ganara en un mes que tuvo tres ventas */
+
+let sueldo = prompt("Ingrese el valor del sueldo");
+
+
+ 
