b0495ec5ff141674e22875ad922c7c03b09020b0
Julian200604 <hector_guillen@soy.sena.edu.co>
2023-07-25 22:27:52 -0500

    se agregaron ejercicios

--- JAVASCRIPT/ejercicio 2/js/script2.js
+++ JAVASCRIPT/ejercicio 2/js/script2.js
@@ -8,7 +8,20 @@ alert("El nuevo sueldo con el aumento es: "+nsueldo); */
 /*Ejercicio 3- Un vendedor que tiene su sueldo base , recibe una comisión del 30% del total de las ventas del mes, el quiere saber
 cuanto ganara en un mes que tuvo tres ventas */
 
-let sueldo = prompt("Ingrese el valor del sueldo");
+/*let sueldo = prompt("Ingrese el valor del sueldo");
+let ventas= prompt("ingrese el valor de las ventas");
+let pventas= (100/30);
+let total = parseInt(sueldo) + parseInt(ventas*pventas);
+
+alert("en un mes ganare "+total);*/
+
+/*ejercicio 4*/
+let compra = prompt("Ingrese el valor de su compra");
+let pventas= (25/100) 
+let descuento = (compra*pventas)
+let Tcompra = parseInt(compra-descuento);
+
+alert("Total a pagar "+Tcompra);
 
 
  
