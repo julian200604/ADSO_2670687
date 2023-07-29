67063a9ce75432aa0621a0f4f4ab28176cf2b069
Julián Guillén <guillen2006@gmail.com>
2023-07-28 22:24:49 -0500

    se agregaron ejercicios 6, 7, 8, y 9

--- JAVASCRIPT/ejercicio 2/js/script2.js
+++ JAVASCRIPT/ejercicio 2/js/script2.js
@@ -33,4 +33,48 @@ alert("El total a ganar en un mes fue "+total);*/
 
 /*ejercicio 6*/
 
+/*let parciNotas= prompt("Escriba le promedio de las tres notas parciales");
+let promedio1= (50/100);
+let parcial= parseInt(parciNotas*promedio1);
+let califinal= prompt("Escriba la calificaión de su examen final");
+let promedio2= (30/100);
+let exafinal= parseInt(califinal*promedio2);
+let trabFinal= prompt("Escriba la calificación de su trabajo final");
+let promedio3= (15/100);
+let califin= parseInt(trabFinal*promedio3);
+ let calimateria= parseInt(parcial+exafinal+califin);
+
+ alert ("Esta seria su nota final "+calimateria)*/
+
+ /*ejercio 7*/
+
+ /*let arDeterminado =prompt("Escriba el precio en que adquirio el producto")
+ let ganancia= (30/100);
+ let total= parseInt(arDeterminado) + parseInt(arDeterminado*ganancia);
+
+ alert("Este seria el precio en que debe vender el producto "+total);*/
+
+ /*Ejercicio 8*/
+
+ /*let kmRecorrido= prompt("agregue los kilometros recorridos ");
+ const costoPorkm=15000;
+ let totalPagoKm=parseInt(kmRecorrido*costoPorkm);
+ let mnRecorridos= prompt("Agregue la cantidad de minutos recorridos");
+ const costoPorMn=2000;
+ let totalPagoMn=parseInt(mnRecorridos*costoPorMn);
+
+ let totalPago=parseInt(totalPagoKm+totalPagoMn);
+
+ alert("su total a pagar es "+totalPago);*/
+
+ /*ejercicio 9*/
+
+ let numero1=prompt("Ponga un número")
+ let numero2=prompt("Ponga un número")
+ let numero3=prompt("Ponga un número")
+
+ let alcuadro1=parseInt(numero1*numero1);
+ let alcuadro2=parseInt(numero2*numero2);
+ let alcuadro3=parseInt(numero3*numero3);
+ alert("Este es el valor al cuadrado de cada número "+alcuadro1 +alcuadro2 +alcuadro3);
  
